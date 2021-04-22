9b1e184ec911475ed409d083601d9eab5c5c5818
Kiarash Farivar <farivar@simba-compute-gpu-2.iel.epfl.ch>
2021-04-22 11:17:52 +0200

    Robust training added

--- demo.py
+++ demo.py
@@ -12,139 +12,116 @@ log.basicConfig(
     ]
 )
 
-from outline.AdvLib import Adversarisal_bench as ab
-from outline.simple_model import simple_conv_Net
-from outline.Get_dataset import CIFAR10_module
-from outline.Measurements import Normal_accuracy, Robust_accuracy
-from outline.utils import print_dict_pretty, add_normalization_layer
+from lib.AdvLib import Adversarisal_bench as ab
+from lib.simple_model import simple_conv_Net
+from lib.Get_dataset import CIFAR10_module
+from lib.Measurements import Normal_accuracy, Robust_accuracy
+from lib.utils import print_measurement_results, print_train_test_val_result, add_normalization_layer
+from lib.Trainer import Robust_trainer
 
 
-# Get the model
-use_simple_model = False
-if use_simple_model:
-    # simple model
-    net = simple_conv_Net()
-    path = './model_checkpoints/simple_conv_non_robust_cifar_epoch_10.pth'
-    net.load_state_dict(torch.load(path))
-    # normalization for inputs in [0,1] 
-    model_mean = (0.5, 0.5 ,0.5)
-    model_std = (0.5, 0.5 ,0.5)
-else:
-    # get resnet18
-    from PyTorch_CIFAR10.cifar10_models.resnet import resnet18
-    net = resnet18(pretrained=True)
-    # normalization for inputs in [0,1]
-    model_mean = (0.4914, 0.4822, 0.4465)
-    model_std = (0.2471, 0.2435, 0.2616)
+def main(args):
+    # Get the model
 
-# add a normalization layer
-net = add_normalization_layer(net, model_mean, model_std)
+    if args.model=='simple':
+        # simple model
+        net = simple_conv_Net()
+        path = './model_checkpoints/simple_conv_non_robust_cifar_epoch_10.pth'
+        net.load_state_dict(torch.load(path))
+        # normalization for inputs in [0,1] 
+        model_mean = (0.5, 0.5 ,0.5)
+        model_std = (0.5, 0.5 ,0.5)
+    elif args.model=='resnet18':
+        # get resnet18
+        from PyTorch_CIFAR10.cifar10_models.resnet import resnet18
+        net = resnet18(pretrained=True)
+        # normalization for inputs in [0,1]
+        model_mean = (0.4914, 0.4822, 0.4465)
+        model_std = (0.2471, 0.2435, 0.2616)
 
-
-
-
-# make sure the data is in [0,1] ! if you use pytorch ToTensor tranform it is already taken care of.
-# note we have already added a normalization layer to our models to adjust them to this data.
-dataset = CIFAR10_module(mean=(0,0,0), std=(1,1,1), data_dir = "./data")
-# prepare and setup the dataset
-dataset.prepare_data()
-dataset.setup()
-
-# define  meaures
-normal_acc = Normal_accuracy()
-robust_acc = Robust_accuracy()
-
-#initialize and send the model to AdvLib
-# This has to be done before defining the attacks (and sensding the model to them) 
-# otherwise the devies and the eval mode won't be set properly !!!
-model_bench = ab(net, device='cuda:2', predictor=lambda x: torch.max(x, 1)[1])
-
-fgsm = torchattacks.FGSM(net, eps=8/255)
-
-apgd = torchattacks.APGD(net, eps=8/255, steps=10)
-
-attacks = [fgsm]
-
-on_train=False
-measurements = [normal_acc, robust_acc]
-results = model_bench.measure(dataset, measurements, attacks, on_train=on_train)
-
-log.info(results)
-
-print_dict_pretty(results, measurements, on_train=on_train)
-
-# TODO
-# 0. a solution for defiing the benchmark before attacks ?
-# 1. check if other attacks work
-# 4. train robust model
-# 5. measure dataset concentration
+    # add a normalization layer
+    net = add_normalization_layer(net, model_mean, model_std)
 
 
 
 
+    # make sure the data is in [0,1] ! if you use pytorch ToTensor tranform it is already taken care of.
+    # note we have already added a normalization layer to our models to adjust them to this data.
+    dataset = CIFAR10_module(mean=(0,0,0), std=(1,1,1), data_dir = "./data")
+    # prepare and setup the dataset
+    dataset.prepare_data()
+    dataset.setup()
+
+    # define  meaures
+    normal_acc = Normal_accuracy()
+    robust_acc = Robust_accuracy()
+
+    #initialize and send the model to AdvLib
+    # This has to be done before defining the attacks (and sending the model to them) 
+    # otherwise the devies and the eval mode won't be set properly !!!
+    # this is weird since the attacks automatically puts the model in eval mode ?!
+    model_bench = ab(net, device='cuda:2', predictor=lambda x: torch.max(x, 1)[1])
 
 
-# # get the dataset concentration
-# all_samples = None
-# concent = ad.measure_dataset_concentration(all_samples)
+    model = net
+    attacks = [torchattacks.FGSM(model, eps=8/255),
+            #torchattacks.BIM(model, eps=8/255, alpha=2/255, steps=7),
+            #torchattacks.CW(model, c=1, kappa=0, steps=1000, lr=0.01),
+            #torchattacks.RFGSM(model, eps=8/255, alpha=4/255, steps=1),
+            #torchattacks.PGD(model, eps=8/255, alpha=2/255, steps=7),
+            #torchattacks.FFGSM(model, eps=8/255, alpha=12/255),
+            #torchattacks.TPGD(model, eps=8/255, alpha=2/255, steps=7),
+            #torchattacks.MIFGSM(model, eps=8/255, decay=1.0, steps=5),
+            #torchattacks.APGD(model, eps=8/255, steps=10),
+            #torchattacks.FAB(model, eps=8/255),
+            #torchattacks.Square(model, eps=8/255),
+            #torchattacks.PGDDLR(model, eps=8/255, alpha=2/255, steps=7),
+        ]
 
-# # get an only non-robust dataset
-# train_set = None
-# non_robuts_train = ad.make_non_robust_dataset(train_set)
 
-# # make a robust dataset ?????
+    if args.mode == 'measuring':
+        on_train=False
+        on_val = False
+        measurements = [normal_acc, robust_acc]
+        results = model_bench.measure_splits(dataset, measurements, attacks, on_train=on_train, on_val=on_val)
+        print_measurement_results(results, measurements, on_train=on_train)
 
-
-
-###################################
-# attacks test
-""" model = net.to('cuda').eval()
-
-atks = [torchattacks.FGSM(model, eps=8/255),
-        #torchattacks.BIM(model, eps=8/255, alpha=2/255, steps=7),
-        #torchattacks.CW(model, c=1, kappa=0, steps=1000, lr=0.01),
-        #torchattacks.RFGSM(model, eps=8/255, alpha=4/255, steps=1),
-        #torchattacks.PGD(model, eps=8/255, alpha=2/255, steps=7),
-        #torchattacks.FFGSM(model, eps=8/255, alpha=12/255),
-        #torchattacks.TPGD(model, eps=8/255, alpha=2/255, steps=7),
-        #torchattacks.MIFGSM(model, eps=8/255, decay=1.0, steps=5),
-        #torchattacks.APGD(model, eps=8/255, steps=10),
-        #torchattacks.FAB(model, eps=8/255),
-        #torchattacks.Square(model, eps=8/255),
-        #torchattacks.PGDDLR(model, eps=8/255, alpha=2/255, steps=7),
-        #torchattacks.DeepFool(model, steps=50, overshoot=0.02),
-        #torchattacks.OnePixel(model, pixels=1, steps=75, popsize=400),
-        #torchattacks.SparseFool(model, steps=20, lam=3, overshoot=0.02),
-       ]
-import time
-from tqdm import tqdm
-
-print("Adversarial Image & Predicted Label")
-
-for atk in atks :
-    
-    print("-"*70)
-    print(atk)
-    
-    correct = 0
-    total = 0
-    
-    for images, labels in tqdm(dataset.test_dataloader()):
+    elif args.mode == 'robust_training':
+        on_val = False
+        measurements = [normal_acc, robust_acc]
+        #initialise optimizer 
+        optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+        loss = torch.nn.CrossEntropyLoss()
+        trainer = Robust_trainer(optimizer, loss)
+        results = model_bench.train_val_test(trainer, 11, dataset, measurements, attacks, train_measure_frequency=2, val_measure_frequency=2)
+        print_train_test_val_result(results, measurements)
         
-        start = time.time()
-        adv_images = atk(images, labels)
-        labels = labels.to('cuda')
-        outputs = model(adv_images)
 
-        _, pre = torch.max(outputs.data, 1)
 
-        total += labels.shape[0]
-        correct += (pre == labels).sum()
 
-        #imshow(torchvision.utils.make_grid(adv_images.cpu().data, normalize=True), [imagnet_data.classes[i] for i in pre])
 
-    print('Total elapsed time (sec) : %.2f' % (time.time() - start))
-    print('Robust accuracy: %.2f %%' % (100 * float(correct) / total))
-    print(f'total{total}  corrrect{correct}') """
+    
+
+
+if __name__ == '__main__':
+    #parse args
+    import argparse
+    parser = argparse.ArgumentParser()
+
+
+    parser.add_argument('model', type=str, choices=['simple', 'resnet18'])
+
+    parser.add_argument('mode', choices=['measuring', 'robust_training'])
+
+    args = parser.parse_args()
+
+    main(args)
+
+
+
+
+
+
+
 
 
